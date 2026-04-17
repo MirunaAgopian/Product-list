34ce0f0afb8f95656db400419aa2e23e0416f00f
Miruna Agopian <agopian.miruna@yahoo.com>
2026-04-17 19:54:51 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { ProductList } from './shared/components/product-list/product-list'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('product_list');
 }
