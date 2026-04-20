e334fe2491d50de263da3102e3300d8b065a9fbc
Miruna Agopian <agopian.miruna@yahoo.com>
2026-04-20 19:08:49 +0200

    enhanced rendering of product list with angular control flow

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+    path = "";
+    btnTxt = "Neues Produkt";
+    ngOnInit(){
+      this.path = "";
+      if(this.path === "detail"){
+        this.btnTxt = "Zurück zur Liste";
+      }
+    }
+}
