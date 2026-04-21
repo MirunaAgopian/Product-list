7ae1eb36ed6b9d512e0a6429ca5c23ebde6b89da
Miruna Agopian <agopian.miruna@yahoo.com>
2026-04-21 19:26:23 +0200

    added routing for the product list details via routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProductDetails } from './shared/components/product-details/product-details';
+import { ProductList } from './shared/components/product-list/product-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: ProductList
+    },
+    {
+        path: "details",
+        component: ProductDetails
+    },
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
-import { ProductList } from './shared/components/product-list/product-list'; 
-import { ProductDetails } from './shared/components/product-details/product-details';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     Header, 
-    ProductList,
-    ProductDetails
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
